e1061b8f4bc3595e72eb0c99fb9a23a4a0972f2a
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-01-24 14:45:58 -0600

    Problem 136 solved, Ugly Numbers

---
+++ UVA/136/generator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+unsigned long isUgly(unsigned long n){
+    if(n == 1)      return 0;
+    if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0){
+        if(n % 2 == 0) n /= 2;
+        else if(n % 3 == 0) n /= 3;
+        else if(n % 5 == 0) n /= 5;
+
+        return isUgly(n);
+    }
+    return n;
+}
+int main() {
+    unsigned long i = 0;
+    int c = 0;
+    for(i = 1; c < 1500; i++) if(!isUgly(i)) c++; 
+
+    printf("%lu", i);
+    return 0;
+}
