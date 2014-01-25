1755836579b4c564e808636ff34de66df5f6251f
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-01-25 06:41:57 -0600

    Problem 10062 solved, Tell me the frequencies

--- UVA/10062/10062.c
+++ UVA/10062/10062.c
@@ -2,32 +2,31 @@
 #include <string.h>
 
 int main(){
-    int c, i = 0, f = 0, max = 1, min = 1;
-    int count[97] = {0};
+    int i = 0, f = 1, max = 0;
+    int count[98] = {0};
+    char line[1010];
 
-    while((c = getchar()) != EOF){
-        if(f) printf("\n");
-        if(c >= 32 && c <= 128){
-            i = 1;
-            f = 0;
-            count[c - 32]++;
-            if(count[c - 32] > max) max = count[c - 32];
-            if(count[c - 32] < min) min = count[c - 32];
-        }else{
-            if(i && (c == '\n' || c == '\r')){
-                for(; min <= max; min++){
-                    int h;
-                    for(h = 96; h >= 0; h--){
-                        if(count[h] == min) printf("%d %d\n", h + 32, count[h]);
-                    }
-                }
-                i = 0;
-                f = 1;
-                memset(count, 0, sizeof(count));
-            }else{
-                break;
+    while(fgets(line, sizeof(line), stdin) != NULL){
+        i = 0;
+        while(line[i] != '\0'){
+            if(line[i] >= 32 && line[i] <= 127){
+                count[(int)line[i] - 32]++;
+                if(count[(int)line[i] - 32] > max) max = count[(int)line[i] - 32];
+                if(count[(int)line[i] - 32] > max) max = count[(int)line[i] - 32];
+            }
+            i++;
+        }
+
+        if(f) f = 0; 
+        else printf("\n");
+
+        for(i = 1; i <= max; i++){
+            int h;
+            for(h = 96; h >= 0; h--){
+                if(count[h] == i) printf("%d %d\n", h + 32, count[h]);
             }
         }
+        memset(count, 0, sizeof(count));
     }
     return 0;
 }
