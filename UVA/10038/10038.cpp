fa0876940a41a5f44f6bca0c39903efe2f108a10
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-04-09 17:12:38 -0500

    Problem accepted, typo error :@

--- UVA/10038/10038.cpp
+++ UVA/10038/10038.cpp
@@ -43,7 +43,7 @@ int main(){
                 break;
             }
         }
-        (flag) ? printf("Jolly\n") : printf("Not Jolly\n");
+        (flag) ? printf("Jolly\n") : printf("Not jolly\n");
     }
     return 0;
 }
